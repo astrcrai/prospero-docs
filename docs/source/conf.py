d21de8063ca8d1992aff03d156c84fdd7e5d661f
itstcros <t.w.crossman@ljmu.ac.uk>
2024-10-03 15:32:13 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = 'images/logo.png'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
